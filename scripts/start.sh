eb27e6d5f2b859d8a686e7b7a64019ffd876966d
FBLGit <fblgit@users.noreply.github.com>
2017-12-04 13:18:51 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-/scripts/hhvm-repo.sh
+HHVM_REPO_ENABLED=${HHVM_REPO_ENABLED:-1}
+if [[ "$HHVM_REPO_ENABLED" == "1" ]]; then
+  /scripts/hhvm-repo.sh
+else
+  sed -i s'/hhvm.repo.authoritative = true//' /etc/hhvm/server.ini
+fi
 exec supervisord -n
