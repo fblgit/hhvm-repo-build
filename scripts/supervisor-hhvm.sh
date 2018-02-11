b75d437415b13106f5bf5e6a4b64cd82dcfc426f
FBLGit <fblgit@users.noreply.github.com>
2018-02-11 13:36:45 +0100

    Update supervisor-hhvm.sh

--- scripts/supervisor-hhvm.sh
+++ scripts/supervisor-hhvm.sh
@@ -3,5 +3,5 @@ HHVM_CFG=${HHVM_CFG:-/etc/hhvm/server.ini}
 if [ -e $HHVM_CFG ]; then
   exec hhvm --mode server -c $HHVM_CFG
 else
-  exec hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000
+  exec hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000 -vServer.AllowRunAsRoot=1
 fi
